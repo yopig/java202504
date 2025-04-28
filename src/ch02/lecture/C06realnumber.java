e847fde08c47f2fe3ea66522142cc3861cfe2d3b
yopig <rrrmr9410@naver.com>
2025-04-28 14:58:18 +0900

    0428(2)

---
+++ src/ch02/lecture/C06realnumber.java
@@ -0,0 +1,15 @@
+package ch02.lecture;
+
+public class C06realnumber {
+    public static void main(String[] args) {
+        // 실수
+        // float   4byte
+        float a = 3.14F;
+        System.out.println(a);
+        // double  8byte
+        double b = 3.14;
+        System.out.println(b);
+        // 더 넓은 범위를 더 촘촘하게 표현함
+        //
+    }
+}
---
+++ src/ch02/lecture/Ccharacher.java
@@ -0,0 +1,24 @@
+package ch02.lecture;
+
+public class Ccharacher {
+    public static void main(String[] args) {
+        // char 문자열
+        // 하나으 문자를 저장하는 자료형
+        // 2bytes
+
+        char a;
+        a = 'a';
+        System.out.println(a);
+        a = 'b';
+        System.out.println(a);
+        a = 'c';
+        System.out.println(a);
+//        a = '한글';
+        System.out.println(a);
+        a = '\'';
+        System.out.println(a);
+        a = '\ud55c';
+        System.out.println(a);
+
+    }
+}
